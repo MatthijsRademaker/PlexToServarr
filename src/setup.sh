415a5d38981ac1ba0a4cabe5841f2d155b38e2c3
Matthijs <matthijsrademaker33@gmail.com>
2024-09-09 11:38:53 +0200

    feat: overwatch

--- src/setup.sh
+++ src/setup.sh
@@ -3,7 +3,6 @@
 # Set variables
 APP_NAME="Overwatch"  # Replace with your application name
 APP_DIR="/usr/local/bin"  # Where the binary will be moved
-CONFIG_DIR="/etc/$APP_NAME"  # Configuration directory
 ENVIRONMENT="production"  # Set the environment (can be changed to development, testing, etc.)
 
 # Step 1: Build the Go application
@@ -16,8 +15,7 @@ sudo mv $APP_NAME $APP_DIR
 
 # Step 3: Move the correct config file to the config directory
 echo "Setting up the configuration file..."
-sudo mkdir -p $CONFIG_DIR
-sudo cp config.$ENVIRONMENT.yml $CONFIG_DIR/config.yml
+sudo cp config.$ENVIRONMENT.yml $APP_DIR/config.$ENVIRONMENT.yml
 
 # Step 4: Create a systemd service file (optional, for auto startup)
 SERVICE_FILE="/etc/systemd/system/$APP_NAME.service"
@@ -30,7 +28,6 @@ After=network.target
 
 [Service]
 ExecStart=$APP_DIR/$APP_NAME
-WorkingDirectory=$CONFIG_DIR
 Restart=always
 Environment=ENVIRONMENT=$ENVIRONMENT
 
